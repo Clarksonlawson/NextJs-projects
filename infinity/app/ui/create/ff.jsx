910b0ebe7854d0f717cc91ea3244f705629f1693
Clarksonlawson <74101434+Clarksonlawson@users.noreply.github.com>
2023-12-06 14:52:37 +0100

    Hello

--- infinity/app/ui/create/ff.jsx
+++ infinity/app/ui/create/ff.jsx
@@ -3,7 +3,7 @@
 import React, { useState } from "react";
 
 import { cn } from "@/lib/utils";
-import { Icons } from "@/components/icons";
+import { Icons } from "@/components/Icons";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
